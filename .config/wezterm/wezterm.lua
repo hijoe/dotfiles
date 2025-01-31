c42c03445529ebe4f9beb2ec3428e8399d4e4161
hijoe <hijoe@hijoes-MBP.attlocal.net>
2025-01-31 08:17:06 -0600

    added avante, updated wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+local projects = require 'projects'
+
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
@@ -13,8 +15,8 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 10
 
 -- my coolnight colorscheme:
 config.colors = {
@@ -27,6 +29,80 @@ config.colors = {
   selection_fg = "#CBE0F0",
   ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
   brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.leader = {
+  key = "a",
+  mods = "CTRL",
+  timeout_milliseconds = 1000
+}
+
+config.keys = {
+  {
+    -- I'm used to tmux bindings, so am using the quotes (") key to
+    -- split horizontally, and the percent (%) key to split vertically.
+    key = '"',
+    -- Note that instead of a key modifier mapped to a key on your keyboard
+    -- like CTRL or ALT, we can use the LEADER modifier instead.
+    -- This means that this binding will be invoked when you press the leader
+    -- (CTRL + A), quickly followed by quotes (").
+    mods = 'CTRL',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '%',
+    mods = 'CTRL',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { 'Left', 15 },
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { 'Down', 15 },
+  },
+  { key = 'k', mods = 'LEADER', action = wezterm.action.AdjustPaneSize { 'Up', 15 } },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { 'Right', 15 },
+  },
+  {
+    key = 'h',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    -- Present in to our project picker
+    action = projects.choose_project(),
+  },
+  {
+    key = 'f',
+    mods = 'LEADER',
+    -- Present a list of existing workspaces
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+  },
+
 }
 
 -- and finally, return the configuration to wezterm
